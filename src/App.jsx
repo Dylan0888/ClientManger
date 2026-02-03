c3172b885e53cb2bf16ba63b0d53f958a9db131f
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-02-03 18:13:19 +0000

    Formatted code in all files. Fixed error of modal not opening from nav by passing correct prop into component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,37 @@
-import React, { useState } from 'react';
-import Nav from './Components/Nav.jsx';
-import TableList from './Components/TableList.jsx';
-import ModalForm from './Components/ModalForm.jsx';
+import React, { useState } from "react";
+import Nav from "./Components/Nav.jsx";
+import TableList from "./Components/TableList.jsx";
+import ModalForm from "./Components/ModalForm.jsx";
 
 function App() {
-
-  const [isOpen , setIsOpen] = useState(false)
-  const [modalMode , setModalMode] = useState("add")
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalMode, setModalMode] = useState("add");
 
   const handleOpen = (mode) => {
-    setIsOpen(true)
-    setModalMode(mode)
-  }
+    setIsOpen(true);
+    setModalMode(mode);
+  };
 
   const handleSubmit = () => {
-    if(modalMode === "add") {
-      console.log("mode = add")
+    if (modalMode === "add") {
+      console.log("mode = add");
     } else {
-      console.log("mode = edit")
+      console.log("mode = edit");
     }
-  }
+  };
 
   return (
     <>
-      <Nav isOpen={()=> handleOpen("add")}/>
-      <TableList handleOpen={handleOpen}/>
-      <ModalForm isOpen={isOpen} onClose={() => setIsOpen(false)} mode={modalMode} onSubmit={() => handleSubmit}/>
+      <Nav handleOpen={handleOpen} />
+      <TableList handleOpen={handleOpen} />
+      <ModalForm
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        mode={modalMode}
+        onSubmit={() => handleSubmit}
+      />
     </>
-  )
+  );
 }
 
-export default App
-
-
-
-
-
+export default App;
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 
-const Nav = ({onOpen}) => {
+const Nav = ({ handleOpen }) => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
-       
         <a className="btn btn-ghost text-xl">Client Manager 3000</a>
       </div>
       <div className="navbar-center ">
         <div className="form-control">
-            <input type="text" placeholder="Search" className="input input-bordered w-48 md:w-auto outline-0 border-2"/>
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-48 md:w-auto outline-0 border-2"
+          />
         </div>
       </div>
       <div className="navbar-end">
-        <a className="btn btn-primary" onClick={onOpen}>Add Client</a>
+        <a className="btn btn-primary" onClick={() => handleOpen("add")}>
+          Add Client
+        </a>
       </div>
     </div>
   );
 };
 
 export default Nav;
-
-
-
