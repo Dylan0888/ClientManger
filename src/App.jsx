4c9ab3b80e6f49b9fddaf4381b26fee1d35ba129
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2026-02-03 16:45:14 +0000

    Added Components folder and Nav.jsx file with code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Nav from './Components/Nav.jsx'
 
 function App() {
 
 
   return (
     <>
+      <Nav />
     <button class="btn w-64 rounded-full">Button</button>
     </>
   )
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+       
+        <a className="btn btn-ghost text-xl">Client Manager 3000</a>
+      </div>
+      <div className="navbar-center ">
+        <div className="form-control">
+            <input type="text" placeholder="Search" className="input input-bordered w-48 md:w-auto outline-0 border-2"/>
+        </div>
+      </div>
+      <div className="navbar-end">
+        <a className="btn btn-primary">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
+
+
+
